b35330d519b7f9553664ed49e13c9e6e955c7eea
Jip J. Dekker <jip@dekker.one>
2025-07-27 16:51:55 +1000

    [PATCH] Use C-style callbacks for FixedAssignmentListener (IPASIR-UP)

--- src/cadical.hpp
+++ src/cadical.hpp
@@ -224,7 +224,7 @@ struct External;
 // Forward declaration of call-back classes. See bottom of this file.
 
 class Learner;
-class FixedAssignmentListener;
+// class FixedAssignmentListener;
 class Terminator;
 class ClauseIterator;
 class WitnessIterator;
@@ -395,7 +395,7 @@ public:
   //   require (VALID)
   //   ensure (VALID)
   //
-  void connect_fixed_listener (FixedAssignmentListener *fixed_listener);
+  void connect_fixed_listener (CFixedAssignmentListener fixed_listener);
   void disconnect_fixed_listener ();
 
   // ====== BEGIN IPASIR-UP ================================================
@@ -1241,12 +1241,12 @@ public:
 // is fixed (for example during inprocessing or due to derived unit
 // clauses).
 
-class FixedAssignmentListener {
-public:
-  virtual ~FixedAssignmentListener () {}
+// class FixedAssignmentListener {
+// public:
+//   virtual ~FixedAssignmentListener () {}
 
-  virtual void notify_fixed_assignment (int) = 0;
-};
+//   virtual void notify_fixed_assignment (int) = 0;
+// };
 
 /*------------------------------------------------------------------------*/
 
