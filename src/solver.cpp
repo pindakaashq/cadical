34386ce92ab1c7362b808ae660219aecbf8059e3
Jip J. Dekker <jip@dekker.one>
2026-02-02 14:26:17 +1100

    [PATCH] resize on Solver::copy

--- src/solver.cpp
+++ src/solver.cpp
@@ -469,7 +469,7 @@ Solver::~Solver () {
 
 /*------------------------------------------------------------------------*/
 
-int Solver::vars () {
+int Solver::vars () const {
   TRACE ("vars");
   REQUIRE_VALID_OR_SOLVING_STATE ();
   int res = external->max_var;
@@ -1796,6 +1796,7 @@ void Solver::copy (Solver &other) const {
   REQUIRE_READY_STATE ();
   REQUIRE (other.state () & CONFIGURING, "target solver already modified");
   internal->opts.copy (other.internal->opts);
+  other.resize(vars ());
   ClauseCopier clause_copier (other);
   traverse_clauses (clause_copier);
   WitnessCopier witness_copier (other.external);
