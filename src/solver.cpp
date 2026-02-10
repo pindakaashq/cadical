9bf0fe5b29bd649ba8f78ee4f971471f95e27414
Jip J. Dekker <jip@dekker.one>
2026-02-10 13:19:26 +1100

    [PATCH] allow usage of `declare_more_variables` during solving
    https://github.com/arminbiere/cadical/issues/159

--- src/solver.cpp
+++ src/solver.cpp
@@ -488,8 +488,10 @@ void Solver::resize (int min_max_var) {
 
 int Solver::declare_more_variables (int number_of_vars) {
   TRACE ("declare_more_variables", number_of_vars);
-  REQUIRE_VALID_STATE ();
-  transition_to_steady_state ();
+  REQUIRE_VALID_OR_SOLVING_STATE ();
+  if (state () != SOLVING) {
+  	transition_to_steady_state ();
+  }
   external->reset_extended ();
   int new_max_var = external->max_var + number_of_vars;
   external->init (new_max_var);
@@ -499,8 +501,10 @@ int Solver::declare_more_variables (int number_of_vars) {
 
 int Solver::declare_one_more_variable () {
   TRACE ("declare_one_more_variable");
-  REQUIRE_VALID_STATE ();
-  transition_to_steady_state ();
+  REQUIRE_VALID_OR_SOLVING_STATE ();
+  if (state () != SOLVING) {
+  	transition_to_steady_state ();
+  }
   external->reset_extended ();
   int new_max_var = external->max_var + 1;
   external->init (new_max_var);
